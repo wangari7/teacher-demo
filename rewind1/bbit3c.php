0a0ada2cb4709c1bc57bf973d07b2b44ff03997a
kdbz <kinyuawallace@gmail.com>
2018-04-25 08:58:15 +0300

    Rewind1 quiz 3c

---
+++ rewind1/bbit3c.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  Consider the following string(bbit3c.txt)
+  It constains names(surname,second_name,first_name),number,country and country code of some individuals
+
+  @Required
+  1. Create a new branch from the develop branch called feature-rewind-1
+  2. Display the following data as follows
+
+      a). first_name second_name,surname (# country_code(0) number[country])
+      b). .......
+      c). .......
+
+    Summary
+     -- Total names : total.
+     -- Total unique countries : total(list of countries - comma separated)
+  3. Commit changes of the new branch
+  4. Merge changes with the development branch
+  5. Push the changes to your repo.
+  6. Submit the github url on google classroom (Check Quiz 1)
+ */
+ ?>
