da517c004c5898ce2dcfaa5fc54b5e165dee62d1
kdbz <kinyuawallace@gmail.com>
2018-04-25 10:12:07 +0300

    Rewind1 quiz 3a

---
+++ rewind1/bbit3a.php
@@ -0,0 +1,26 @@
+<?php
+/********************************************************************************************************************
+  Consider the following string(bbit3a.txt)                                                                         *
+  It constains names(IP address,surname,second_name,first_name),number,country and country code of some individuals *
+                                                                                                                    *
+  @Required                                                                                                         *
+  1. Create a new branch from the develop branch called feature-rewind-13a                                          *
+  2. Display the following data as follows (NB: first_name can be clicked to open the persons IP on a new tab)      *
+                                                                                                                    *
+      a). first_name second_name,surname (# country_code(0)number[country])                                         *
+      b). .......                                                                                                   *
+      c). .......                                                                                                   *
+      (For example                                                                                                  *
+        a) Mosiro Olesaningo Chirchir(# +254 (0) 760 387 482[KENYA])                                                *
+      )                                                                                                             *
+                                                                                                                    *
+    Summary                                                                                                         *
+     -- Total names : total.                                                                                        *
+     -- Total unique countries : total(list of countries - comma separated)                                         *
+  3. Commit changes of the new branch                                                                               *
+  4. Merge changes with the development branch                                                                      *
+  5. Push the changes to your repo.                                                                                 *
+  6. Submit the github url on google classroom (Check Quiz 1 - 3A)                                                  *
+ ********************************************************************************************************************/
+
+ ?>
